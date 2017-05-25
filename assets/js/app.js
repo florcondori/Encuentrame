6ab3163b5f179b85cec438d1dd248d85b8b5d4fa
flor condori <condorihuamanif@gmail.com>
2017-05-25 17:48:57 -0500

    zoom 20

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
-app
+function initMap() {
+  var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 20,
+    center: laboratoriaLima
+  });
+}
